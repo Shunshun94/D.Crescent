cacd26073a461df22a38c4f15f3ddf1ff17ff9ad
Shunshun94 <shunshun9460@gmail.com>
2018-05-27 00:34:17 +0900

    fix checks

--- main/check/effectCheck.js
+++ main/check/effectCheck.js
@@ -60,7 +60,16 @@ io.github.shunshun94.HiyokoCross.EffectCheck = class extends com.hiyoko.componen
 				`<h3 class="${this.clazz}-header"><span class="${this.id}-name ${this.clazz}-name">エフェクト</span>` +
 				`<button id="${this.id}-toggle">開閉</button></h3>`);
 		this.$html.append(this.effects.map((skill, i) => {return this.generateEffectDom(skill, i)}).join(`<hr class="${this.clazz}-border"/>`));
-		this.$html.append(`<button id="${this.id}-exec">エフェクト行使 (判定は行われず侵蝕率のみ上昇します)</button>`);
+		this.getElementsByClass(`effect-name`).each((i, $dom) => {
+			$($dom).text(this.effects[i].name);
+		});
+		this.getElementsByClass(`effect-note`).each((i, $dom) => {
+			$($dom).text(this.effects[i].notes || '');
+		});
+		this.getElementsByClass(`effect-spec-type`).each((i, $dom) => {
+			$($dom).text(this.effects[i].type || '');
+		});
+		this.$html.append(`<button id="${this.id}-exec">エフェ クト行使 (判定は行われず侵蝕率のみ上昇します)</button>`);
 	}
 
 	useEffects(e) {
@@ -89,17 +98,17 @@ io.github.shunshun94.HiyokoCross.EffectCheck = class extends com.hiyoko.componen
 	generateEffectDom (skill, num) {
 		return 	`<div class="${this.id}-effect ${this.clazz}-skill" id="${this.id}-skill-${num}">` +
 				`<input value="${num}" type="checkbox" class="${this.id}-effect-check" />` +
-				`<span class="${this.id}-effect-name ${this.clazz}-skill-name">${skill.name}</span>` +
+				`<span class="${this.id}-effect-name ${this.clazz}-skill-name"></span>` +
 				`${skill.check ? '(' + skill.check + ')' : ''} (` +
 				`Lv. <span class="${this.id}-effect-level ${this.clazz}-skill-level">${skill.level}</span>)`+
 				`<div class="${this.id}-effect-spec">` +
 					`タイミング:<span class="${this.id}-effect-spec-timing">${skill.timing || ''}</span> / ` +
 					`対象:<span class="${this.id}-effect-spec-target">${skill.target || ''}</span> /` +
 					`射程:<span class="${this.id}-effect-spec-range">${skill.range || ''}</span> /` +
-					`技能:<span class="${this.id}-effect-spec-type">${skill.type || ''}</span> /` +
+					`技能:<span class="${this.id}-effect-spec-type"></span> /` +
 					`難易度:<span class="${this.id}-effect-spec-judge">${skill.judge || ''}</span> /` +
 					`侵蝕率:<span class="${this.id}-effect-spec-cost">${skill.cost || ''}</span>` +
-				`</div><div class="${this.id}-effect-note">${skill.notes || ''}</div></div>`;
+				`</div><div class="${this.id}-effect-note"></div></div>`;
 	}
 };
 
